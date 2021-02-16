67005635eb8878883b82493d4eb410fa0ebcf733
William P <hello@metallicgloss.com>
2021-02-16 22:53:14 +0000

    Convert text to bundles, exit on control C, renamed interface elements.

--- DiscussionNetApplication/src/main/java/work/universitycourse/comp1549/Modules/InterfaceManager.java
+++ DiscussionNetApplication/src/main/java/work/universitycourse/comp1549/Modules/InterfaceManager.java
@@ -1,10 +1,14 @@
 package work.universitycourse.comp1549.Modules;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.lang.reflect.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 /**
  *
@@ -14,6 +18,25 @@ import javax.swing.JFrame;
  * @author William Phillips
  */
 public class InterfaceManager {
+    // Define program icon.
+    public static ImageIcon programIcon = new ImageIcon(InterfaceManager.class.getResource("/icon.png"));
+
+    public static Action exitProgram = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Exit application.
+            System.exit(0);
+        }
+    };
+     
+    public static void detectExitRequest(JPanel currentWindow) {
+        // Target input map of "C" and "CTRL" pressed at the same time.
+        currentWindow.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_DOWN_MASK), "exitApplicationRequest");
+        
+        // Map action of "exitProgram" to input map defined above of "exitApplicationRequest".
+        currentWindow.getActionMap().put("exitApplicationRequest", exitProgram);   
+    }
+     
     public static void changeWindow(JFrame currentWindow, JFrame targetWindow) {
         // Close current JFrame.
         currentWindow.dispose();
